a3053d2e2ef5f052332fc2f867d5912bd66ac451
Saket Sourav <saketsourav3@gmail.com>
2025-02-02 23:44:52 +0530

    utility function

--- app/utils/removeBg.ts
+++ app/utils/removeBg.ts
@@ -1,1 +1,11 @@
-console.log("utility function to remove background from an image");
+import { removeBackground } from "@imgly/background-removal";
+
+export const removeBg = async (imageUrl: string) => {
+  try {
+    const imageBlob = await removeBackground(imageUrl);
+    return URL.createObjectURL(imageBlob);
+  } catch (error) {
+    console.error("Error removing background:", error);
+    return null;
+  }
+};
