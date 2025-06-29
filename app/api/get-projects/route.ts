83d7ba0b73b59933ef227f9548590c09e701ef66
Knightsaket007 <saketsourav3@gmail.com>
2025-06-29 23:54:52 +0530

    api tested from postman

---
+++ app/api/get-projects/route.ts
@@ -0,0 +1,21 @@
+// /api/load-editor.ts
+import { NextResponse } from "next/server";
+import { connectToDB } from "@/lib/mongodb";
+import EditorState from "@/app/models/EditorState";
+
+export async function GET(req: Request) {
+  try {
+    const { id } = await req.json();
+
+    // if (!editorId) return NextResponse.json({ error: "Missing editorId" }, { status: 400 });
+
+    await connectToDB();
+    const data = await EditorState.find({ _id: id });
+     console.log("data", data)
+
+    return NextResponse.json(data);
+  } catch (err) {
+    console.error("Failed to load editor state:", err);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
