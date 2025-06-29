d9c1cf10819b018f62a01627de61c8be841cd11f
Knightsaket007 <saketsourav3@gmail.com>
2025-06-30 00:21:03 +0530

    get projects api working fine

--- app/api/get-projects/route.ts
+++ app/api/get-projects/route.ts
@@ -4,20 +4,12 @@ import { connectToDB } from "@/lib/mongodb";
 import EditorState from "@/app/models/EditorState";
 
 export async function GET(req: Request) {
-    try {
+    const url = new URL(req.url);
+    const id = url.searchParams.get("id");
+    if (!id) return NextResponse.json({ error: "Missing ID in query" }, { status: 400 });
 
-        //=-=-=-= Next Logic for user active id or not=-=-=-//
-        //=-=-=-= Next Logic for user active id or not=-=-=-//
-
-        const { id } = await req.json();
-        if (!id) return NextResponse.json({ error: "Missing editor ID" }, { status: 400 });
-        await connectToDB();
-        const data = await EditorState.find({ _id: id });
-        console.log("data...", data)
-
-        return NextResponse.json(data);
-    } catch (err) {
-        console.error("Failed to load editor state:", err);
-        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
-    }
+    await connectToDB();
+    const data = await EditorState.find({ userId: id });
+    return NextResponse.json(data);
 }
+
