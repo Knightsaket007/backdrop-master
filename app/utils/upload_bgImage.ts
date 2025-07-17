995af0278409bcf7338a8d66dedeefd4fd397f45
Knightsaket007 <saketsourav3@gmail.com>
2025-07-18 00:00:10 +0530

    new

---
+++ app/utils/upload_bgImage.ts
@@ -0,0 +1,11 @@
+import { uploadImage } from "./upload";
+
+import React from 'react'
+
+const upload_bgImage = () => {
+  return (
+    <div>upload_bgImage</div>
+  )
+}
+
+export default upload_bgImage
