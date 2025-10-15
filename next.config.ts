994a2dc43657439c1eee2a4b78ec4ab3d996031b
Knightsaket007 <saketsourav3@gmail.com>
2025-10-15 16:48:25 +0530

    chnage in config

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,22 @@
 /** @type {import('next').NextConfig} */
-import type { Configuration } from "webpack";
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true, // ✅ Ensure app router is enabled
-     webAssembly: true,
-    workerThreads: true,
-  },
-    webpack: (config: Configuration): Configuration => {
-    config.experiments = {
-      ...(config.experiments || {}),
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    return config;
-  },
   images: {
-    domains: ["pixabay.com", "images.pexels.com"], // ✅ Allow Pixabay images in Next.js
-    
+    remotePatterns: [
+      { protocol: 'https', hostname: 'pixabay.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/editor/:path*",
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+        ],
+      },
+    ];
   },
 };
 
