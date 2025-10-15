b2af503c0757699fda070180e4f453ccec17ddd1
Knightsaket007 <saketsourav3@gmail.com>
2025-10-15 12:32:13 +0530

    config webpack

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
+import type { Configuration } from "webpack";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     appDir: true, // ✅ Ensure app router is enabled
+     webAssembly: true,
+    workerThreads: true,
+  },
+    webpack: (config: Configuration): Configuration => {
+    config.experiments = {
+      ...(config.experiments || {}),
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
   },
   images: {
     domains: ["pixabay.com", "images.pexels.com"], // ✅ Allow Pixabay images in Next.js
